aa08c9fb6f15a27bf18798ae9f3fdc8e34ebc73a
spiddy <dimitris.kapanidis@harbur.io>
2021-03-25 13:27:23 +0100

    wip

--- src/ui/tooltips/TooltipWithShortcut.tsx
+++ src/ui/tooltips/TooltipWithShortcut.tsx
@@ -17,10 +17,11 @@ interface TooltipWithShortcutProps {
 function TooltipWithShortcut({ description, shortcut, children, placement }: TooltipWithShortcutProps) {
   return (
     <HtmlTooltip title={
-      <div className="bg-gray-800 p-1 border rounded-md border-gray-700 px-2 text-xs text-gray-200">
-          {description}
-          {shortcut && <span className="text-xs text-gray-200"> · <code className="bg-gray-700 inset-0 px-1 py-0.5 rounded-sm text-xs">G</code> then <code className="bg-gray-700 inset-0 px-1 py-0.5 rounded-sm text-xs">{shortcut}</code></span>}
+      description ? <div className="bg-gray-800 p-1 border rounded-md border-gray-700 px-2 text-xs text-gray-200">
+        {description}
+        {shortcut && <span className="text-xs text-gray-200"> · <code className="bg-gray-700 inset-0 px-1 py-0.5 rounded-sm text-xs">G</code> then <code className="bg-gray-700 inset-0 px-1 py-0.5 rounded-sm text-xs">{shortcut}</code></span>}
       </div>
+        : <></>
     } aria-label="add" enterDelay={500} leaveDelay={200} placement={placement}>
       {children}
     </ HtmlTooltip>
